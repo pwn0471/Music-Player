cb4b8df9af795ccb96ec2e15dca4f48e0c576081
pawankumar <pwnkumar0471@gmail.com>
2025-12-30 23:56:38 +0530

    making playlist

--- script.js
+++ script.js
@@ -5,6 +5,91 @@ let ctrlicon = document.getElementById("ctrlicon");
 let currTime = document.getElementById("currTime");
 let duration = document.getElementById("duration");
 
+let playlist = [
+    {
+        title:"Ghera Hua",
+        artist:"By Arijit Singh and Armaan Khan",
+        src:"media/Gehra-Hua-by-Irshad-Kamil(PagalWorldi.com.co).mp3",
+        img:"media/ghrahua.jpg"
+    },
+    {
+        title:"Ez-Ez",
+        artist:" By Diljit Dosanjh and Hanumankind",
+        src:"media/Ez-Ez (PenduJatt.Com.Se).mp3",
+        img:"media/download (2).jpg"
+    },
+    {
+        title:"Dhurandhar Arabic",
+        artist:"By Hanumankind, Jasmine Sandlas, Sudhir Yaduvanshi, Muhammad Sadiq, and Ranjit Kaur",
+        src:"media/Dhurandhar Arabic (PenduJatt.Com.Se).mp3",
+        img:"media/download (2).jpg"
+    },
+    {
+        title:"Shararat",
+        artist:"By Madhubanti Bagchi and Jasmine Sandlas",
+        src:"media/Shararat (PenduJatt.Com.Se).mp3",
+        img:"media/download.jpg"
+    },
+    {
+        title:"Ishq Jalakar - Karvaan",
+        artist:"By Shahzad Ali, Subhadeep Das Chowdhury, and Armaan Khan",
+        src:"media/Ishq Jalakar - Karvaan (PenduJatt.Com.Se).mp3",
+        img:"media/download (3).jpg"
+    }
+];
+
+
+
+let songIndex = 0;
+
+function loadSong(index){
+    song.src = playlist[index].src;
+
+    document.querySelector("h1").textContent = playlist[index].title;
+    document.querySelector("p").textContent = playlist[index].artist;
+    document.querySelector(".song-img").src = playlist[index].img;
+    song.load();
+
+}
+
+loadSong(songIndex);
+
+let nextBtn = document.getElementById("next");
+let prevBtn = document.getElementById("prev");
+
+nextBtn.onclick = function () {
+  songIndex++;
+  if (songIndex >= playlist.length) {
+    songIndex = 0;
+  }
+  loadSong(songIndex);
+  song.play();
+  ctrlicon.classList.add("fa-pause");
+  ctrlicon.classList.remove("fa-play");
+};
+
+prevBtn.onclick = function () {
+  songIndex--;
+  if (songIndex < 0) {
+    songIndex = playlist.length - 1;
+  }
+  loadSong(songIndex);
+  song.play();
+  ctrlicon.classList.add("fa-pause");
+  ctrlicon.classList.remove("fa-play");
+};
+
+
+song.addEventListener("ended", function () {
+  songIndex++;
+  if (songIndex >= playlist.length) {
+    songIndex = 0;
+  }
+  loadSong(songIndex);
+  song.play();
+});
+
+
 
 song.onloadedmetadata = function(){
     progres.max = song.duration;
